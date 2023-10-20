677d8a41ccb1f0a7112e06d6dc729de8430eefbd
ebouhid <eduardobouhid.neto@gmail.com>
2023-10-20 09:51:37 -0300

    Included a wait command between experiments.

--- segmentation/training_script.sh
+++ segmentation/training_script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # 8 bands (All + NDVI) composition
 nohup python deeplab.py --composition 8 7 6 5 4 3 2 1 \
 --composition_name "All+NDVI" \
@@ -5,11 +7,15 @@ nohup python deeplab.py --composition 8 7 6 5 4 3 2 1 \
 --encoder "efficientnet-b0" \
 --experiment_name "Encoder_Comparison"
 
+wait
+
 nohup python deeplab.py --composition 8 7 6 5 4 3 2 1 \
 --composition_name "All+NDVI" \
 --batch_size 32 \
 --encoder "resnet34" \
 --experiment_name "Encoder_Comparison"
+
+wait
 
 nohup python deeplab.py --composition 8 7 6 5 4 3 2 1 \
 --composition_name "All+NDVI" \
@@ -23,17 +29,23 @@ nohup python deeplab.py --composition 6 5 1 \
 --encoder "efficientnet-b0" \
 --experiment_name "Encoder_Comparison"
 
+wait
+
 nohup python deeplab.py --composition 6 5 1 \
 --composition_name "651" \
 --batch_size 32 \
 --encoder "resnet34" \
 --experiment_name "Encoder_Comparison"
 
+wait
+
 nohup python deeplab.py --composition 6 5 1 \
 --composition_name "651" \
 --batch_size 32 \
 --encoder "resnet101" \
 --experiment_name "Encoder_Comparison"
+
+wait
 
 # Single band (6) composition
 nohup python deeplab.py --composition 6 \
@@ -42,14 +54,20 @@ nohup python deeplab.py --composition 6 \
 --encoder "efficientnet-b0" \
 --experiment_name "Encoder_Comparison"
 
+wait
+
 nohup python deeplab.py --composition 6 \
 --composition_name "6" \
 --batch_size 32 \
 --encoder "resnet34" \
 --experiment_name "Encoder_Comparison"
 
+wait
+
 nohup python deeplab.py --composition 6 \
 --composition_name "6" \
 --batch_size 32 \
 --encoder "resnet101" \
 --experiment_name "Encoder_Comparison"
+
+wait
